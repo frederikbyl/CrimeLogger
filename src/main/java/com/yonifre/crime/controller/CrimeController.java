4922daee1fcb52693a4d7e998ab64e770364e481
Frederik Byl <frederik.byl@kbc.be>
2017-07-09 08:49:04 +0200

    added services

--- src/main/java/com/yonifre/crime/controller/CrimeController.java
+++ src/main/java/com/yonifre/crime/controller/CrimeController.java
@@ -1,8 +1,13 @@
 package com.yonifre.crime.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yonifre.crime.model.Crime;
@@ -17,5 +22,27 @@ public class CrimeController {
 		
 		return new Crime();
 	}
+	
+	@RequestMapping(path="/crimes",  method = RequestMethod.GET)
+	public  List<Crime> getAllCrimes() {
+		ArrayList<Crime> crimes = new ArrayList<Crime>();
+		
+		return crimes;
+	}
+	
+	@RequestMapping(path="/crimes/{category}",  method = RequestMethod.GET)
+	public  List<Crime> getAllCrimesForCategory(@PathVariable String category) {
+		ArrayList<Crime> crimes = new ArrayList<Crime>();
+		
+		return crimes;
+	}
+	
+	@RequestMapping(path="/crimes",  method = RequestMethod.GET)
+	public  List<Crime> getAllCrimesForCategory(@RequestParam(value="long", defaultValue="-1") Float longitude, @RequestParam(value="long", defaultValue="-1") Float latitude ) {
+		ArrayList<Crime> crimes = new ArrayList<Crime>();
+		
+		return crimes;
+	}
+
 
 }
