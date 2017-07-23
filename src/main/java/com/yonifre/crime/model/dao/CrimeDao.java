f409fbe88da103fed4c408b07db3ed15eaef930c
Frederik Byl <frederik.byl@kbc.be>
2017-07-23 18:52:28 +0200

    disabled db for a while

--- src/main/java/com/yonifre/crime/model/dao/CrimeDao.java
+++ src/main/java/com/yonifre/crime/model/dao/CrimeDao.java
@@ -6,11 +6,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-@Entity
+//@Entity
 public class CrimeDao {
 
-	@Id
-	@GeneratedValue
+//	@Id
+//	@GeneratedValue
 	private Long id;
 	private String category;
 	private Long numberOfPeopleInvolved;
--- src/main/java/com/yonifre/crime/repository/CrimeRepository.java
+++ src/main/java/com/yonifre/crime/repository/CrimeRepository.java
@@ -1,9 +1,5 @@
 package com.yonifre.crime.repository;
 
-import org.springframework.data.repository.CrudRepository;
-
-import com.yonifre.crime.model.dao.CrimeDao;
-
-public interface CrimeRepository extends CrudRepository<CrimeDao, Long>  {
+public interface CrimeRepository {//extends CrudRepository<CrimeDao, Long>  {
 
 }
